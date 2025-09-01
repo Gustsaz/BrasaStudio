f09b668a409e230c4e7d2aa720ddb01c21fc3e36
Gustavo Almeida Zuchini <gustavo.zuchini@etec.sp.gov.br>
2025-09-01 10:50:50 -0300

    Update modelo3d.js

--- modelo3d.js
+++ modelo3d.js
@@ -1,7 +1,16 @@
-// modelo3d.js
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 
+
+'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+import { GLTFLoader } from 
+
+'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.
+
+js';
+import { OrbitControls } from 
+
+'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitContr
+
+ols.js';
 
 // Cena, câmera e renderizador
 const scene = new THREE.Scene();
